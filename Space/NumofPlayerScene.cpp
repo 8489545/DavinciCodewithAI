4bae8babbce4fa7eac3cd059df675969a681942d
8489545 <rebob9545@gmail.com>
2020-07-03 16:39:35 +0900

    플레이어 수 선택 씬 이미지 추가

--- Space/NumofPlayerScene.cpp
+++ Space/NumofPlayerScene.cpp
@@ -11,6 +11,17 @@ NumofPlayerScene::~NumofPlayerScene()
 
 void NumofPlayerScene::Init()
 {
+	m_BG = Sprite::Create(L"Painting/NumofPlayer/BG.png");
+	m_BG->SetPosition(1920 / 2, 1080 / 2);
+
+	m_TwoPlayer = Sprite::Create(L"Painting/NumofPlayer/TwoPlayer.png");
+	m_TwoPlayer->SetPosition(1920 / 2, 300);
+
+	m_ThreePlayer = Sprite::Create(L"Painting/NumofPlayer/ThreePlayer.png");
+	m_ThreePlayer->SetPosition(1920 / 2, 500);
+
+	m_FourPlayer = Sprite::Create(L"Painting/NumofPlayer/FourPlayer.png");
+	m_FourPlayer->SetPosition(1920 / 2, 700);
 }
 
 void NumofPlayerScene::Release()
@@ -23,4 +34,8 @@ void NumofPlayerScene::Update(float deltaTime, float Time)
 
 void NumofPlayerScene::Render()
 {
+	m_BG->Render();
+	m_TwoPlayer->Render();
+	m_ThreePlayer->Render();
+	m_FourPlayer->Render();
 }
--- Space/NumofPlayerScene.h
+++ Space/NumofPlayerScene.h
@@ -1,6 +1,10 @@
 #pragma once
 class NumofPlayerScene : public Scene
 {
+	Sprite* m_BG;
+	Sprite* m_TwoPlayer;
+	Sprite* m_ThreePlayer;
+	Sprite* m_FourPlayer;
 public:
 	NumofPlayerScene();
 	~NumofPlayerScene();
