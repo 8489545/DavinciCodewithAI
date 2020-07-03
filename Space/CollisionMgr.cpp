cbce347742e7622f22c8045cf712b7cbc4b6bc91
8489545 <rebob9545@gmail.com>
2020-07-03 14:19:53 +0900

    마우스 버튼 충돌 함수 변경

--- Space/CollisionMgr.cpp
+++ Space/CollisionMgr.cpp
@@ -13,14 +13,13 @@ CollisionMgr::~CollisionMgr()
 
 bool CollisionMgr::MouseWithBoxSize(Object* obj)
 {
-	Vec2 Mouse = INPUT->GetMousePos();
-	if (Mouse.x > obj->m_Position.x - obj->m_Size.x
-		&& Mouse.x < obj->m_Position.x + obj->m_Size.x / 2
-		&& Mouse.y > obj->m_Position.y - obj->m_Size.y
-		&& Mouse.y < obj->m_Position.y + obj->m_Size.y / 2)
-	{
+	POINT Mouse;
+
+	Mouse.x = INPUT->GetMousePos().x;
+	Mouse.y = INPUT->GetMousePos().y;
+
+	if (PtInRect(&obj->m_Collision, Mouse))
 		return true;
-	}
 
 	return false;
 }
