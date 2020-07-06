59e673673b6443bcca65fb5f6b8b1613bd83e76d
김 동규 <rebob9545@gmail.com>
2020-07-06 20:04:03 +0900

    페이즈로 다음 행동 구분

--- Space/GameMgr.h
+++ Space/GameMgr.h
@@ -2,6 +2,17 @@
 #include<random>
 #include<iterator>
 #include<algorithm>
+enum class PHASE
+{
+	BlockDist,		//블록 분배
+	SetOrder,		//순서 정하기
+	ImportBlock,	//블록 가져오기
+	BlockFit,		//블록 맞추기
+	KeepFit,		//계속 맞추기
+	StopFit,		//그만 맞추기
+	TurnClock,		//순서 넘기기
+	NONE
+};
 class GameMgr : public Singleton<GameMgr>
 {
 public:
@@ -9,6 +20,10 @@ public:
 	~GameMgr();
 
 	int m_NumOfPlayer;
+	PHASE m_GamePhase;
+
+	PHASE GetGamePhase() { return m_GamePhase; }
+	void SetGamePhase(PHASE phase) { m_GamePhase = phase; }
 
 	std::vector<Block*> m_AllBlock;
 
