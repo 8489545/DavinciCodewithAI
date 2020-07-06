a7c48f66a5917164d6fec7423c2e721bef76759f
김 동규 <rebob9545@gmail.com>
2020-07-06 21:54:29 +0900

    UI추가

--- Space/GameScene.cpp
+++ Space/GameScene.cpp
@@ -16,19 +16,33 @@ void GameScene::Init()
 	m_Table = Sprite::Create(L"Painting/Game/Table.png");
 	m_Table->SetPosition(1920 / 2, 1080 / 2);
 
+	m_UIBlockDist = Sprite::Create(L"Painting/Game/UI/Dist.png");
+	m_UIBlockDist->SetPosition(1920 / 2, 100);
+
 	GameMgr::GetInst()->BlockInitSetting();
+	GameMgr::GetInst()->SetGamePhase(PHASE::BlockDist);
 }
 
 void GameScene::Release()
 {
 }
 
+void GameScene::BlockDist()
+{
+}
+
 void GameScene::Update(float deltaTime, float Time)
 {
+	if (GameMgr::GetInst()->GetGamePhase() == PHASE::BlockDist)
+	{
+		BlockDist();
+	}
 }
 
 void GameScene::Render()
 {
 	m_BG->Render();
 	m_Table->Render();
+	if (GameMgr::GetInst()->GetGamePhase() == PHASE::BlockDist)
+		m_UIBlockDist->Render();
 }
--- Space/GameScene.h
+++ Space/GameScene.h
@@ -3,12 +3,16 @@ class GameScene : public Scene
 {
 	Sprite* m_BG;
 	Sprite* m_Table;
+
+	Sprite* m_UIBlockDist;
 public:
 	GameScene();
 	~GameScene();
 
 	void Init();
-	void Release();
+	void Release(); 
+
+	void BlockDist();
 
 	void Update(float deltaTime, float Time);
 	void Render();
